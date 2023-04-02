8332a909f2f857b4fb54f4ea3472cd197fcabd71
vasu68m <balendravyas96m@gmail.com>
2023-04-02 09:38:00 +0100

    quiz mounted on git

---
+++ index.js
@@ -0,0 +1,91 @@
+const questions= [
+{
+    'que': "which of the following is a markup language?",
+    'a': "html",
+    'b': "css",
+    'c': "ruby",
+    'd': "js",
+    'correct answer': "a"
+},
+
+{
+    'que': "who is the strongest avenger?",
+    'a': "hulk",
+    'b': "thor",
+    'c': "captain america",
+    'd': "iron-man",
+    'correct answer': "d"
+},
+
+{
+    'que': "thors's hammer is madeup of?",
+    'a': "vibranium",
+    'b': "nth metal",
+    'c': "stainless steel",
+    'd': "uru",
+    'correct answer': "d"
+}
+
+]
+
+let index = 0;
+let total = questions.length;
+let right = 0, wrong = 0;
+const quesBox = document.getElementById("queBox");
+const optionInputs = document.querySelectorAll(".options")
+
+const loadQuestion = () =>{
+    
+    if(index==total){
+       return endQuiz()
+    }
+    reset();
+    const data = questions[index]
+    // console.log(data)
+    quesBox.innerText = `${index+1} ${data.que}`;
+    optionInputs[0].nextElementSibling.innerText = data.a;
+    optionInputs[1].nextElementSibling.innerText = data.b;
+    optionInputs[2].nextElementSibling.innerText = data.c;
+    optionInputs[3].nextElementSibling.innerText = data.d;
+}
+
+const submitQuiz = () => {
+    const data = questions[index];
+    const ans = getAnswer()
+    if(ans==data.correct){
+         right++;
+    }else{
+        wrong++;
+    }
+    index++;
+    loadQuestion();
+    return data;
+
+}
+
+const getAnswer = () =>{
+    let answer;
+    optionInputs.forEach(
+        (input)=>{
+        if (input.checked){
+            // console.log("yes")
+            // console.log(input.value)
+            answer= input.value;
+        }
+    }
+    )
+    return answer;
+}
+
+const reset = () =>{
+    optionInputs.forEach((input)=>{
+        input.checked = false;
+    })
+}
+
+const endQuiz =()=>{
+    document.getElementById("box").innerHTML = `<h3> Thanks for Participating</h3>`
+    // <h2> ${right}/${total} are correct </h2>
+}
+
+loadQuestion();
